329c46679c85cf2806f34ca96eecc9e5a4610d9a
ANGI498045 <angi498045@gmail.com>
2024-09-02 18:05:16 +0200

    unban

---
+++ commands/commandes/unban.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("unban")
+    .setDescription("Débannit un membre")
+    .addStringOption(option =>
+        option.setName("membre")
+        .setDescription("Le membre à débannir")
+        .setRequired(true)
+    ),
+    async execute(interaction) {
+        const user = interaction.options.getString("membre")
+
+        try {
+            await interaction.guild.bans.remove(user);
+            await interaction.reply("Membre débannit !")
+        } catch (error) {
+            console.error(error);
+            interaction.reply("Erreur en débanissant le membre")
+        }
+    },
+};
