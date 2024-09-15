fea9120d95bfdd21446efaa4a5b763ec1bbdefac
ANGI498045_ <angi498045@gmail.com>
2024-09-15 18:50:03 +0200

    online.js

---
+++ commands/commandes/online.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("online")
+    .setDescription("Vérifie si le bot est online"),
+    async execute(interaction) {
+        try {
+            await interaction.reply("Je suis bien connecté !")
+        } catch (error) {
+            console.error(error)
+            await interaction.reply("Bot connecté, mais problème avec la commande...")
+        }
+    },
+}
