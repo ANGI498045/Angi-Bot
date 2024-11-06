ea777fa21372a3e379b8dd04e8ef0ff49fa3fa79
ANGI498045_ <angi498045@gmail.com>
2024-11-06 12:28:28 +0100

    arrival

--- commands/moderation/arrival.js
+++ commands/moderation/arrival.js
@@ -5,7 +5,7 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("arrival")
     .setDescription("Envoie un message d'arrivée")
-    //.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addUserOption(option =>
         option.setName("membre")
         .setDescription("Le membre qui est arrivé")
@@ -18,7 +18,7 @@ module.exports = {
             .addFields({name: "Nouveau Membre", value: `${user} a rejoint le serveur ! Bienvenue !`})
             .setColor(0x0099ff)
             .setTimestamp()
-            await interaction.channel.send("arrival1");
+            await interaction.channel.send({embeds: [embed]});
         } catch (error) {
             console.error(error);
         }
