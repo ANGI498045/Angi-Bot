1ba918fbe8e89ff57e22a8ebfd6712d1229e8209
ANGI498045 <angi498045@gmail.com>
2025-01-14 09:58:46 +0100

    warn

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js")
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("warn")
         .setDescription("Avertir un membre")
-        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+        .setDefaultMemberPermissions(PermissionFlagsBits.MuteMembers)
         .addUserOption(options =>
             options.setName("membre")
             .setDescription("Le membre à avertir")
@@ -12,5 +12,16 @@ module.exports = {
         .addStringOption(options => 
             options.setName("raison")
             .setDescription("La raison pour laquelle tu l'avertis")
-        )
+        ),
+        async execute(interaction) {
+            const user = interaction.options.getUser("membre");
+            const reason = interaction.options.getString("raison");
+            const embed = new EmbedBuilder()
+                .setTitle("Avertissement")
+                .setDescription(`Le membre \`${user}\` a reçu un avertissement. Raison: \`${reason}\` `)
+                .setColor(0xF68A11)
+                .setTimestamp()
+
+            await interaction.reply({embeds: [embed]});
+        }
 }
