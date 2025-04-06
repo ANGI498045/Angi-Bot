0ea1e0e0baf0d07e278ab9eb09cceb10ac5d8019
ANGI498045 <angi498045@gmail.com>
2025-04-06 12:20:30 +0200

    bump

--- commands/utility/bump.js
+++ commands/utility/bump.js
@@ -1,16 +1,16 @@
 const { EmbedBuilder } = require("@discordjs/builders");
 const { SlashCommandBuilder } = require("discord.js");
 const {channelBot} = require("../../json/channels.json");
-const embed = new EmbedBuilder()
-    .setTitle("Rappel de Bump")
-    .addFields({name: "C'est l'heure du bump !", value: "Allez-y, bumpez !"})
-    .setColor(0x0099ff)
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("bump")
         .setDescription("Valide ton bump (tout abus sera punni)"),
     async execute(interaction) {
     const channel = interaction.guild.channels.cache.get(channelBot);
+    const embed = new EmbedBuilder()
+        .setTitle("Rappel de Bump")
+        .addFields({name: "C'est l'heure du bump !", value: "Allez-y, bumpez !"})
+        .setColor(0x0099ff)
     interaction.reply(`Merci pour ton bump ! Voici un point de bonus.`);
     const delay = (delayInms) => {
         return new Promise(resolve => setTimeout(resolve, delayInms));
